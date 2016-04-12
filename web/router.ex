14290311961f93ecebfb055616bc090245c67993
Dean Chouinard <deanchouinard@gmail.com>
2016-04-11 23:02:10 -0400

    authentication

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Todo.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Todo.Auth, repo: Todo.Repo
   end
 
   pipeline :api do
