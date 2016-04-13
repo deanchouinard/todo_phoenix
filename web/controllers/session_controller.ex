a9683daaf0ed26a9bacf93c2712b6c9dae2278d7
Dean Chouinard <deanchouinard@gmail.com>
2016-04-12 23:06:42 -0400

    user account links

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -10,12 +10,18 @@ defmodule Todo.SessionController do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
-        |> redirect(to:, page_path(conn, :index)
+        |> redirect(to: page_path(conn, :index))
       {:error, _reason, conn} ->
         conn
         |> put_flash(:error, "Invalid username/password")
-        |> render("new.htnl")
+        |> render("new.html")
     end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Todo.Auth.logout()
+    |> redirect(to: page_path(conn, :index))
   end
 
 end
